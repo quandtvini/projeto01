e9a5ed5e8f12fcd11564f4c5ee7fc5bc341dff0e
quandtvini <149084204+quandtvini@users.noreply.github.com>
2023-11-09 11:44:22 -0300

    adicionamos os aquivos css, js, html

---
+++ sketch.js
@@ -0,0 +1,14 @@
+eCanvas(400, 400);
+   background("black");
+}
+function setup() {
+  creat
+function draw() {
+  
+  stroke("white");
+fill("red");
+  
+      if(mouseIsPressed){
+  rect(mouseX,mouseY,20,20);
+  }
+}
